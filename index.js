b62f7e9e6394c2a0be362b46315398db460d3239
Doug Knight <dknight@getaroom.com>
2021-07-20 15:47:16 -0500

    start adding account balance

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const parseArgs = require ('minimist')
 
 const About = require('./lib/about');
+const Accounts = require('./lib/accounts');
 
 const args = parseArgs(process.argv);
 const command = args._[2]
@@ -11,7 +12,10 @@ switch(command) {
     break;
   case 'help':
     About.help();
-
+    break;
+  case 'account':
+    Accounts.exec(args);
+    break;
   default:
     console.log(`unrecognized command "${command}"`);
 };
